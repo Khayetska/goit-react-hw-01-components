eab89297991b6d269261bd17d43d474eb8ba5b6c
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-05-05 01:06:56 +0300

    Did first task: made profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { Profile } from './Profile/Profile';
+import userData from 'user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
   );
 };
