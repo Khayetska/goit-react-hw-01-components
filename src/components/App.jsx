d7fbc2b528d4d8c154bc593558c6d3024c872645
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-05-06 01:29:35 +0300

    Did fourth task: made transactions table

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import userData from 'user.json';
 import statisticsData from 'data.json';
 import friends from 'friends.json';
+import transactions from 'transactions.json';
 
 export const App = () => {
   return (
@@ -19,6 +21,8 @@ export const App = () => {
       <Statistics title="Upload stats" stats={statisticsData} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th className={css.transactionHistory__head}>Type</th>
+          <th className={css.transactionHistory__head}>Amount</th>
+          <th className={css.transactionHistory__head}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td className={css.transactionHistory__cell}>{type}</td>
+              <td className={css.transactionHistory__cell}>{amount}</td>
+              <td className={css.transactionHistory__cell}>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
