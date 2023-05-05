b993de919215db8049b8c9b083db32125530876d
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-05-05 23:31:42 +0300

    Did second task: made statistics board

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 import userData from 'user.json';
+import statisticsData from 'data.json';
 
 export const App = () => {
   return (
@@ -11,6 +13,8 @@ export const App = () => {
         avatar={userData.avatar}
         stats={userData.stats}
       />
+
+      <Statistics title="Upload stats" stats={statisticsData} />
     </>
   );
 };
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statList}>
+        {stats.map(stat => {
+          return (
+            <li className={css.item} key={stat.id}>
+              <span className={css.label}>{stat.label}</span>
+              <span className={css.percentage}>{stat.percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array.isRequired,
+};
